3e65b78c419522d0478dd8c7c05d828116af255b
Ruslan Balkovyy <rbalkovyy@gmail.com>
2022-10-03 19:00:27 +0300

    Initial commit

---
+++ src/main/java/com/rufus/App.java
@@ -0,0 +1,13 @@
+package com.rufus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
